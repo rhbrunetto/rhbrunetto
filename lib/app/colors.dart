f79c9c999810ddf6f3b1486a3d4c648c097bbd94
Ricardo Brunetto <ricardo@rhbrunetto.com>
2023-04-14 01:22:27 -0300

    wip

--- lib/app/colors.dart
+++ lib/app/colors.dart
@@ -6,4 +6,6 @@ class RPColors {
   static const dark = Color(0xff282424);
   static const orangeHighlight = Color(0xffff4c1c);
   static const yellowHighlight = Color(0xffffd404);
+  static const beigeBackground = Color(0xffe0d4c4);
+  static const greyShadow = Color(0xffe0e0e0);
 }
--- lib/features/home/widgets/right_section.dart
+++ lib/features/home/widgets/right_section.dart
@@ -6,7 +6,40 @@ class RightSection extends StatelessWidget {
   const RightSection({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Container(
-        color: RPColors.purpleHighlight,
+  Widget build(BuildContext context) => Stack(
+        children: [
+          Container(
+            color: RPColors.beigeBackground,
+          ),
+          const _Bubble(image: ' '),
+        ],
+      );
+}
+
+class _Bubble extends StatelessWidget {
+  const _Bubble({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) => const DecoratedBox(
+        decoration: ShapeDecoration(
+          shape: CircleBorder(),
+          shadows: [
+            BoxShadow(
+              color: Color(0xff111111),
+              blurRadius: 8,
+              blurStyle: BlurStyle.outer,
+            )
+          ],
+        ),
+        child: CircleAvatar(
+          radius: 48,
+          backgroundColor: Colors.white,
+          child: Icon(Icons.abc),
+        ),
       );
 }
